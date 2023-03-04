2487af053c662233048e1d6954cbc111252bcb9a
ZiYooh <natsucomi42@gmail.com>
2023-03-04 06:03:00 +0000

    Build: 봇 시작 스크립트 파일 생성
    봇 시작 스크립트 파일 생성

---
+++ start.sh
@@ -0,0 +1,6 @@
+# Install requirements
+pip install psutil beautifulsoup4 rich requests
+pip3 install psutil beautifulsoup4 rich requests
+
+# Start process background
+nohup python3 main.py & > /dev/null
